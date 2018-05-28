cca0369dce7c31fd07f5d9f4409ff13fbef05672
Md Riad Hossain <riad.hossain@apsissolutions.com>
2018-05-28 14:20:23 +0600

    Initial Core File Upload

---
+++ www/js/app.js
@@ -0,0 +1,54 @@
+/*
+ * Please see the included README.md file for license terms and conditions.
+ */
+
+
+// This file is a suggested starting place for your code.
+// It is completely optional and not required.
+// Note the reference that includes it in the index.html file.
+
+
+/*jslint browser:true, devel:true, white:true, vars:true */
+/*global $:false, intel:false app:false, dev:false, cordova:false */
+
+
+// For improved debugging and maintenance of your app, it is highly
+// recommended that you separate your JavaScript from your HTML files.
+// Use the addEventListener() method to associate events with DOM elements.
+
+// For example:
+
+// var el ;
+// el = document.getElementById("id_myButton") ;
+// el.addEventListener("click", myEventHandler, false) ;
+
+
+
+// The function below is an example of the best way to "start" your app.
+// This example is calling the standard Cordova "hide splashscreen" function.
+// You can add other code to it or add additional functions that are triggered
+// by the same event or other events.
+
+function onAppReady() {
+    if( navigator.splashscreen && navigator.splashscreen.hide ) {   // Cordova API detected
+        navigator.splashscreen.hide() ;
+    }
+}
+document.addEventListener("app.Ready", onAppReady, false) ;
+function printImage(){
+    window.DatecsPrinter.listBluetoothDevices(
+      function (devices) {
+        window.DatecsPrinter.connect(devices[0].address, 
+          function() {
+            alert('Connected!');
+          },
+          function() {
+            alert(JSON.stringify(error));
+          }
+        );
+      },
+      function (error) {
+        alert(JSON.stringify(error));
+      }
+    );
+}
